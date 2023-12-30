7daeb0b695b9f5822f0cb831518c9aec469a3524
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2023-12-30 16:01:32 -0300

    Update ChunkAreaManager.java

--- src/main/java/net/vulkanmod/render/chunk/ChunkAreaManager.java
+++ src/main/java/net/vulkanmod/render/chunk/ChunkAreaManager.java
@@ -180,7 +180,7 @@ public class ChunkAreaManager {
 
     public void resetQueues() {
         for(ChunkArea chunkArea : this.chunkAreasArr) {
-            chunkArea.drawBuffers().clear();
+            chunkArea.resetQueue();
         }
     }
 
@@ -193,5 +193,4 @@ public class ChunkAreaManager {
             chunkArea.releaseBuffers();
         }
     }
-
 }
